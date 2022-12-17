a7bd0fe9029642b881cb4066b01f835d67de9136
fatih <muhammadfatihmaulana434@gmail.com>
2022-12-17 10:54:32 +0700

    update dikit

--- app/src/main/java/com/studydicoding/submission_aplikasi_sederhana/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/studydicoding/submission_aplikasi_sederhana/ui/detail/DetailActivity.kt
@@ -1,16 +1,14 @@
 package com.studydicoding.submission_aplikasi_sederhana.ui.detail
 
+import android.os.Build.VERSION
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.studydicoding.submission_aplikasi_sederhana.constant.Constant
 import com.studydicoding.submission_aplikasi_sederhana.data.Plant
 import com.studydicoding.submission_aplikasi_sederhana.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
-
-    companion object {
-        const val PLANT_DATA = "plant"
-    }
 
     private lateinit var binding: ActivityDetailBinding
 
@@ -18,20 +16,27 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        supportActionBar?.hide()
 
-        val dataPlant = intent.getParcelableExtra<Plant>(PLANT_DATA) as Plant
+        val dataPlant = if (VERSION.SDK_INT >= 33) {
+            intent.getParcelableExtra(Constant.PLANT_DATA, Plant::class.java)
+        } else {
+            intent.getParcelableExtra(Constant.PLANT_DATA)
+        }
 
-        Glide.with(this).load(dataPlant.image).into(binding.imgDetailPlant)
+        if (dataPlant != null) {
+            Glide.with(this).load(dataPlant.image).into(binding.imgDetailPlant)
+        }
         binding.apply {
-            tvDetailNamePlant.text = dataPlant.name
-            tvDetailNameBotanical.text = dataPlant.botanicalName
-            tvDescPlant.text = dataPlant.desc
+            if (dataPlant != null) {
+                tvDetailNamePlant.text = dataPlant.name
+                tvDetailNameBotanical.text = dataPlant.botanicalName
+                tvDescPlant.text = dataPlant.desc
+            }
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
+        onBackPressedDispatcher.onBackPressed()
         return super.onSupportNavigateUp()
     }
 }
